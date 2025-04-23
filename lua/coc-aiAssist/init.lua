d3afa06eefcb8ff7c34fbc08b6412ceb3afddcf1
sika7 <91676209+sika7@users.noreply.github.com>
2025-04-23 12:52:33 +0900

    luaのロジックをリファクタ

--- lua/coc-aiAssist/init.lua
+++ lua/coc-aiAssist/init.lua
@@ -1,5 +1,8 @@
 local M = {}
 
+-- ユーティリティモジュールをロード
+local window = require('coc-aiAssist.window')
+
 local function cocChannelId()
   for _, chan in ipairs(vim.api.nvim_list_chans()) do
     if chan["mode"] == "rpc" and chan["argv"] and string.find(chan["argv"][1], "node") then
@@ -14,131 +17,20 @@ local function myNotice(noticeName, value)
   vim.fn.rpcnotify(channelId, noticeName, value)
 end
 
--- ユーティリティモジュールをロード
--- local window = require('coc-assist.window')
-function M.selectAndPreviewWindow(noticeName, title,  prompt, jsonStr)
+function M.selectWindow(noticeName, title, prompt, jsonStr)
   local items = vim.fn.json_decode(jsonStr)
+  window.selectAndPreviewWindow(title, prompt, items, function(item)
+    myNotice(noticeName, item.value)
+  end)
+end
 
-  local picker = require("snacks.picker")
-  -- local items = {
-  --   {
-  --     text = "オプション1",
-  --     value = "value1",
-  --     preview = {
-  --       text = "これはオプション1のプレビューです。",
-  --       ft = "markdown",
-  --     },
-  --   },
-  --   {
-  --     text = "オプション2",
-  --     value = "value2",
-  --     preview = {
-  --       text = "これはオプション2のプレビューです。",
-  --       ft = "markdown",
-  --     },
-  --   },
-  -- }
 
-  picker.pick({
-    prompt = prompt,
-    title = title,
-    items = items,
-    format = "text",
-    layout = "default",
-    preview = "preview",
-    -- layout = "dropdown",
-    -- search = function(_, item)
-    --   return ""
-    -- end,
-    -- live = true,
-    -- supports_live = false,
-    win = {
-      -- input window
-      input = {
-        keys = {
-          ["/"] = "toggle_focus",
-          ["<C-Down>"] = { "history_forward", mode = { "i", "n" } },
-          ["<C-Up>"] = { "history_back", mode = { "i", "n" } },
-          ["<C-c>"] = { "cancel", mode = "i" },
-          ["<C-w>"] = { "<c-s-w>", mode = { "i" }, expr = true, desc = "delete word" },
-          ["<CR>"] = { "confirm", mode = { "n", "i" } },
-          ["<Down>"] = { "list_down", mode = { "i", "n" } },
-          ["<Esc>"] = "cancel",
-          ["<S-Tab>"] = { "list_up", mode = { "i", "n" } },
-          ["<Tab>"] = { "list_down", mode = { "i", "n" } },
-          ["<Up>"] = { "list_up", mode = { "i", "n" } },
-          ["<c-b>"] = { "preview_scroll_up", mode = { "i", "n" } },
-          ["<c-d>"] = { "list_scroll_down", mode = { "i", "n" } },
-          ["<c-f>"] = { "preview_scroll_down", mode = { "i", "n" } },
-          ["<c-j>"] = { "list_down", mode = { "i", "n" } },
-          ["<c-k>"] = { "list_up", mode = { "i", "n" } },
-          ["<c-n>"] = { "list_down", mode = { "i", "n" } },
-          ["<c-p>"] = { "list_up", mode = { "i", "n" } },
-          ["<c-u>"] = { "list_scroll_up", mode = { "i", "n" } },
-          ["?"] = "toggle_help_input",
-          ["G"] = "list_bottom",
-          ["gg"] = "list_top",
-          ["j"] = "list_down",
-          ["k"] = "list_up",
-          ["q"] = "close",
-        },
-        b = {
-          minipairs_disable = true,
-        },
-      },
-      -- result list window
-      list = {
-        keys = {
-          ["/"] = "toggle_focus",
-          ["<2-LeftMouse>"] = "confirm",
-          ["<CR>"] = "confirm",
-          ["<Down>"] = "list_down",
-          ["<Esc>"] = "cancel",
-          ["<S-CR>"] = { { "pick_win", "jump" } },
-          ["<Tab>"] = "list_down",
-          ["<Up>"] = "list_up",
-          ["<c-j>"] = "list_down",
-          ["<c-k>"] = "list_up",
-          ["<c-n>"] = "list_down",
-          ["<c-p>"] = "list_up",
-          ["<c-c>"] = "close",
-          ["<c-u>"] = "list_scroll_up",
-          ["?"] = "toggle_help_list",
-          ["G"] = "list_bottom",
-          ["gg"] = "list_top",
-          ["i"] = "focus_input",
-          ["j"] = "list_down",
-          ["k"] = "list_up",
-          ["q"] = "close",
-          ["zb"] = "list_scroll_bottom",
-          ["zt"] = "list_scroll_top",
-          ["zz"] = "list_scroll_center",
-        },
-        wo = {
-          conceallevel = 2,
-          concealcursor = "nvc",
-        },
-      },
-      -- preview window
-      preview = {
-        keys = {
-          ["<Esc>"] = "cancel",
-          ["q"] = "close",
-          ["i"] = "focus_input",
-        },
-      },
-    },
-    confirm = function(pickerInstance, item)
-      myNotice(noticeName, item.value)
-      pickerInstance:close()
-    end,
-  })
 
-  -- picker.pick("test", {
-  --   prompt = "[]",
-  --   sources = {},
-  --   focus = "input",
-  -- })
+function M.selectAndPreviewWindow(noticeName, title, prompt, jsonStr)
+  local items = vim.fn.json_decode(jsonStr)
+  window.selectAndPreviewWindow(title, prompt, items, function(item)
+    myNotice(noticeName, item.value)
+  end)
 end
 
 -- function M.test()
---
+++ lua/coc-aiAssist/window.lua
@@ -0,0 +1,147 @@
+local M = {}
+
+-- pickerを任意の形で再利用できる
+
+local picker = require("snacks.picker")
+
+local winSetting = {
+  -- input window
+  input = {
+    keys = {
+      ["/"] = "toggle_focus",
+      ["<C-Down>"] = { "history_forward", mode = { "i", "n" } },
+      ["<C-Up>"] = { "history_back", mode = { "i", "n" } },
+      ["<C-c>"] = { "cancel", mode = "i" },
+      ["<C-w>"] = { "<c-s-w>", mode = { "i" }, expr = true, desc = "delete word" },
+      ["<CR>"] = { "confirm", mode = { "n", "i" } },
+      ["<Down>"] = { "list_down", mode = { "i", "n" } },
+      ["<Esc>"] = "cancel",
+      ["<S-Tab>"] = { "list_up", mode = { "i", "n" } },
+      ["<Tab>"] = { "list_down", mode = { "i", "n" } },
+      ["<Up>"] = { "list_up", mode = { "i", "n" } },
+      ["<c-b>"] = { "preview_scroll_up", mode = { "i", "n" } },
+      ["<c-d>"] = { "list_scroll_down", mode = { "i", "n" } },
+      ["<c-f>"] = { "preview_scroll_down", mode = { "i", "n" } },
+      ["<c-j>"] = { "list_down", mode = { "i", "n" } },
+      ["<c-k>"] = { "list_up", mode = { "i", "n" } },
+      ["<c-n>"] = { "list_down", mode = { "i", "n" } },
+      ["<c-p>"] = { "list_up", mode = { "i", "n" } },
+      ["<c-u>"] = { "list_scroll_up", mode = { "i", "n" } },
+      ["?"] = "toggle_help_input",
+      ["G"] = "list_bottom",
+      ["gg"] = "list_top",
+      ["j"] = "list_down",
+      ["k"] = "list_up",
+      ["q"] = "close",
+    },
+    b = {
+      minipairs_disable = true,
+    },
+  },
+  -- result list window
+  list = {
+    keys = {
+      ["/"] = "toggle_focus",
+      ["<2-LeftMouse>"] = "confirm",
+      ["<CR>"] = "confirm",
+      ["<Down>"] = "list_down",
+      ["<Esc>"] = "cancel",
+      ["<S-CR>"] = { { "pick_win", "jump" } },
+      ["<Tab>"] = "list_down",
+      ["<Up>"] = "list_up",
+      ["<c-j>"] = "list_down",
+      ["<c-k>"] = "list_up",
+      ["<c-n>"] = "list_down",
+      ["<c-p>"] = "list_up",
+      ["<c-c>"] = "close",
+      ["<c-u>"] = "list_scroll_up",
+      ["?"] = "toggle_help_list",
+      ["G"] = "list_bottom",
+      ["gg"] = "list_top",
+      ["i"] = "focus_input",
+      ["j"] = "list_down",
+      ["k"] = "list_up",
+      ["q"] = "close",
+      ["zb"] = "list_scroll_bottom",
+      ["zt"] = "list_scroll_top",
+      ["zz"] = "list_scroll_center",
+    },
+    wo = {
+      conceallevel = 2,
+      concealcursor = "nvc",
+    },
+  },
+  -- preview window
+  preview = {
+    keys = {
+      ["<Esc>"] = "cancel",
+      ["q"] = "close",
+      ["i"] = "focus_input",
+    },
+  },
+}
+-- メイン機能
+function M.selectWindow(title, prompt, items, callback)
+  picker.pick({
+    prompt = prompt,
+    title = title,
+    items = items,
+    format = "text",
+    layout = "select",
+    preview = "preview",
+    -- layout = "dropdown",
+    -- search = function(_, item)
+    --   return ""
+    -- end,
+    -- live = true,
+    -- supports_live = false,
+    win = winSetting,
+    confirm = function(pickerInstance, item)
+      callback(item)
+      pickerInstance:close()
+    end,
+  })
+end
+
+function M.selectAndPreviewWindow(title, prompt, items, callback)
+  -- local items = {
+  --   {
+  --     text = "オプション1",
+  --     value = "value1",
+  --     preview = {
+  --       text = "これはオプション1のプレビューです。",
+  --       ft = "markdown",
+  --     },
+  --   },
+  --   {
+  --     text = "オプション2",
+  --     value = "value2",
+  --     preview = {
+  --       text = "これはオプション2のプレビューです。",
+  --       ft = "markdown",
+  --     },
+  --   },
+  -- }
+
+  picker.pick({
+    prompt = prompt,
+    title = title,
+    items = items,
+    format = "text",
+    layout = "default",
+    preview = "preview",
+    -- layout = "dropdown",
+    -- search = function(_, item)
+    --   return ""
+    -- end,
+    -- live = true,
+    -- supports_live = false,
+    win = winSetting,
+    confirm = function(pickerInstance, item)
+      callback(item)
+      pickerInstance:close()
+    end,
+  })
+end
+
+return M
