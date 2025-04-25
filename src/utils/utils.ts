fdd4b008839043627a52e7fe3074744ba1f564d5
sika7 <91676209+sika7@users.noreply.github.com>
2025-04-25 11:47:48 +0900

    不要な関数を削除

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -82,30 +82,6 @@ export function removeMarkdownFormat(text: string) {
   return text.replace(/^```\w*\n|```$/g, "").trim();
 }
 
-export async function yankText(text: string) {
-  // レスポンスをyankレジスタに入れる
-  await workspace.nvim.call("setreg", ["*", text]);
-  logger.info("Response copied to clipboard (yank register)");
-}
-
-export async function writeText(text: string) {
-  if (text == "") return;
-
-  const doc = await workspace.document;
-  const position = await window.getCursorPosition();
-  const range = Range.create(position, position);
-  const edit: TextEdit = { range, newText: text };
-  await doc.applyEdits([edit]);
-}
-
-export async function insertInformation(text: string) {
-  // OKを選択した場合、テキストを挿入
-  const result = await window.showInformationMessage("Insert?", "yes", "no");
-  if (result === "yes") {
-    writeText(text);
-  }
-}
-
 // ビジュアルモードで選択中のテキストを取得
 export async function selectedText() {
   const doc = await workspace.document;
@@ -124,27 +100,3 @@ export async function selectedText() {
   return context;
 }
 
-export function makePrompt(question: string, code: string | null) {
-  // 最終的なプロンプトを構築
-  let prompt = question;
-  if (code && code.trim() !== "") {
-    prompt = `${question}\n\nSelected code:\n\`\`\`\n${code}\n\`\`\``;
-  }
-  return prompt;
-}
-
-export async function getBufferIdFromWindowId(
-  windowId: number,
-): Promise<number> {
-  const nvim = workspace.nvim;
-  try {
-    const bufferId = await nvim.call("winbufnr", [windowId]);
-    return bufferId as number;
-  } catch (error) {
-    logger.error(
-      `ウィンドウID ${windowId} からバッファーIDの取得に失敗しました:`,
-      error,
-    );
-    return -1;
-  }
-}
